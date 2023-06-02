95302a557b30b1626e2536a37e2eb2625c60e4ef
ourystd <amadoury313@gmail.com>
2023-06-02 04:13:55 +0000

    favicon update

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "OuryScript",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
 };
 
 export const LOCALE = ["fr-FR", "en-EN"]; // set to [] to use the environment default
